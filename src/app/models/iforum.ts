ba4622b089152f637486484b97d9d65f85d5a1e7
233409-leonardo-daniel <233409@ids.upchiapas.edu.mx>
2024-11-11 11:23:53 -0600

    forum

--- src/app/models/iforum.ts
+++ src/app/models/iforum.ts
@@ -11,17 +11,8 @@ export interface IForum {
     creation_date: string;
     id_user: number;
     members: number;
-
-    // name: string;
-    // description: string;
-    // background_image_url: string;
-    // image_url: string;
-    // education_level: string;
-    // privacy: string;
-    // password: string;
-    // id_forum: number;
-    // creation_date: string;
-    // user_name: string;
-    // id_user: number;
-    // users_count: number;
+    background_image_url: string;
+    image_url: string;
+    user_name: string;
+    users_count: number;
 }
